ad4c6a3e129dd122db01560c29f2c94db1eaa759
9 Dots <9dots@F124s-MBP.socal.rr.com>
2020-08-03 16:04:17 -0700

    update index

--- keyboard-levels/teach1.js
+++ keyboard-levels/teach1.js
@@ -95,23 +95,4 @@ $(function(){
 
 	});
 
-	/*$(".cloud").mouseenter(function(){
-	  	$(this).animate({left: "-300px"}, 2000);
-	  	$(this).addClass("done");
-	  	let win = true;
-	 	$( ".cloud" ).each(function( index ) {
-		  if (!($(this).hasClass("done"))) {
-		  	win = false;
-		  }
-		});
-		if (win) {
-			$("#myCanvas").css("background-color", "#A9EAFF");
-			$("h1").css("visibility", "visible");
-			$("h1").css("opacity", "1");
-			$("#o3").css("visibility", "visible");
-			$("#o3").css("opacity", "1");
-		}
-	});*/
-
-
 });
