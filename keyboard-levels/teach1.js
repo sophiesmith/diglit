fcac3b471151a51187299d37d1071912ca05049c
9 Dots <9dots@F124s-MBP.socal.rr.com>
2020-08-03 15:10:37 -0700

    working mockup

--- keyboard-levels/teach1.js
+++ keyboard-levels/teach1.js
@@ -1,7 +1,7 @@
 $(function(){
 
 	$(document).keypress(function(e){
-	  if (e.key == 'f') {
+	  if (e.key == 'f' && $('#group1').hasClass("active")) {
 	  	$(".flower1").each(function() {
 	  		if (!($(this).hasClass("done"))) {
 	  			$(this).css("visibility", "visible");
@@ -12,7 +12,6 @@ $(function(){
 	  	});
 
 	  	let done = true;
-
 	  	$(".flower1").each(function() {
 	  		if (!($(this).hasClass("done"))) {
 	  			done = false;
@@ -20,10 +19,12 @@ $(function(){
 	  	});
 	  	if (done) {
 	  		$("#group2").css("opacity", 1);
+	  		$("#group2").addClass("active")
+	  		$("#group1").removeClass("active");
 	  	}
 	  }
 
-	  else if (e.key == 'g') {
+	  else if (e.key == 'g' && $('#group2').hasClass("active")) {
 	  	$(".flower2").each(function() {
 	  		if (!($(this).hasClass("done"))) {
 	  			$(this).css("visibility", "visible");
@@ -32,6 +33,64 @@ $(function(){
 	  			return false;
 	  		} 
 	  	});
+
+	  	let done = true;
+	  	$(".flower2").each(function() {
+	  		if (!($(this).hasClass("done"))) {
+	  			done = false;
+	  		}
+	  	});
+	  	if (done) {
+	  		$("#group3").css("opacity", 1);
+	  		$("#group3").addClass("active")
+	  		$("#group2").removeClass("active");
+	  	}
+	  }
+
+	  else if (e.key == 'h' && $('#group3').hasClass("active")) {
+	  	$(".flower3").each(function() {
+	  		if (!($(this).hasClass("done"))) {
+	  			$(this).css("visibility", "visible");
+	  			$(this).css("opacity", "1");
+	  			$(this).addClass("done");
+	  			return false;
+	  		} 
+	  	});
+
+	  	let done = true;
+	  	$(".flower3").each(function() {
+	  		if (!($(this).hasClass("done"))) {
+	  			done = false;
+	  		}
+	  	});
+	  	if (done) {
+	  		$("#group4").css("opacity", 1);
+	  		$("#group4").addClass("active")
+	  		$("#group3").removeClass("active");
+	  	}
+	  }
+
+	  else if (e.key == 'j' && $('#group4').hasClass("active")) {
+	  	$(".flower4").each(function() {
+	  		if (!($(this).hasClass("done"))) {
+	  			$(this).css("visibility", "visible");
+	  			$(this).css("opacity", "1");
+	  			$(this).addClass("done");
+	  			return false;
+	  		} 
+	  	});
+
+	  	let done = true;
+	  	$(".flower4").each(function() {
+	  		if (!($(this).hasClass("done"))) {
+	  			done = false;
+	  		}
+	  	});
+	  	if (done) {
+	  		$("#group3").removeClass("active");
+	  		$("#sun").css("visibility", "visible");
+			$("#sun").css("opacity", "1");
+	  	}
 	  }
 
 	});
